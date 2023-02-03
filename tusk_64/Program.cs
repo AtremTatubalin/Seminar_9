27d41acfcff724c5a25ad5fde8e9c1f6904aabce
Artyom <tatubalin.artem@mail.ru>
2023-02-03 22:19:50 +0800

    tusk_64

---
+++ tusk_64/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.Clear();
+
+int GetUserN(string massege)
+{
+    Console.Write(massege);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+void RowNumbers(int N)
+{
+    if(N == 1)
+    {
+        Console.WriteLine(N);
+        return;
+    }
+    Console.Write(N + ", ");
+    RowNumbers(N - 1);
+}
+
+int N = GetUserN("Введите границу ряда: ");
+RowNumbers(N);
